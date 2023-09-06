3f83ffded20f77014c203379b39e1209f05fdf6d
Abraham E. Tavarez <abeincwireless@gmail.com>
2023-09-06 19:54:15 -0400

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const jsxEngine = require('jsx-view-engine')
 
 // Variables
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // config
 app.set('view engine', 'jsx');
